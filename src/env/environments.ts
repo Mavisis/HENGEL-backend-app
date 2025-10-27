0755fc50a57baff1acdc13b83074f7412839e4e7
Marc Harinck. <harinckmarc@gmail.com>
2025-10-27 12:43:31 +0100

    working

--- src/env/environments.ts
+++ src/env/environments.ts
@@ -3,7 +3,6 @@
 // ===============================================
 // Copy this to environment.ts and environment.prod.ts
 export const environment = {
-  production: false,
-  apiUrl: 'https://your-api-url.com/api',
-  appUrl: 'https://app.queagent.nl'
+  production: true,
+  apiUrl: '/api/v1'
 };
